8af34aa6f203498636a23e645bd739522c39d9d3
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-07 19:52:23 +0900

    feat : 프로젝트 작성, webview 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'main_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,7 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const ,
+      home: const MainScreen(),
     );
   }
 }
