5661146ad0121e5179f736926aa8209c4b5bb9a4
hwangtaewook <87569211+hwangtaewook@users.noreply.github.com>
2024-04-07 20:49:46 +0900

    feat : 옵션 메뉴

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -8,11 +11,43 @@ class MainScreen extends StatefulWidget {
 }
 
 class _MainScreenState extends State<MainScreen> {
+  final WebViewController _webViewController = WebViewController()
+    ..setJavaScriptMode(JavaScriptMode.unrestricted)
+    ..loadRequest(Uri.parse('https://flutter.dev'));
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('나만의 웹브라우저'),
+        title: const Text('나만의 웹브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected: (value) {
+              print(value);
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www,geogle.com',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www,naver.com',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www,kakao.com',
+                child: Text('카카오'),
+              ),
+            ],
+          ),
+        ],
+      ),
+      body: WebViewWidget(
+        controller: _webViewController,
       ),
     );
   }
